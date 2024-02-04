7df9637e3a65a17c3355d7958e8bd53324c60bc0
rajeev Chapagain <rajeevchapagain01@gmail.com>
2024-02-04 13:14:07 -0500

    some craxy changes are made

--- src/Component/Home/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Home = () => {
-  return <div>Home This ispush for branch capabilities</div>;
-};
-
-export default Home;
---
+++ src/assets/Pages/pageone.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Pageone = () => {
+  return <div>P</div>;
+};
+
+export default Pageone;
