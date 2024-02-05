636d71eb2f534e946c55f6b1b8e9cb7f210df7be
rajeev Chapagain <rajeevchapagain01@gmail.com>
2024-02-04 22:55:12 -0500

    I am being pushed

---
+++ src/newUpdate/update.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const update = () => {
+  return <div>I am update and this update is necessary</div>;
+};
+
+export default update;
---
+++ src/newUpdate/updateTwo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const updateTwo = () => {
+  return <div>updateTwo</div>;
+};
+
+export default updateTwo;
